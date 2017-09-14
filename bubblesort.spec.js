29fc2ac3e0a36613f49d484cf4efc34c4f5435cc
Shelby Rackley <shelbyrackley@Shelbys-MacBook-Pro.local>
2017-09-14 11:33:29 -0400

    Commenting

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -4,4 +4,12 @@ describe('Bubble Sort', function(){
   });
 });
 
+// Bubble Sort
+// Loop over elements
+// swap anything that's out of order
+// repeat until no more swaps
 
+// Merge Sort
+// Divide array until in the smallest bits
+// Sort those subarrays then pick two and merge/sort them
+// Repeat until sorted
